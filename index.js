d58346a77d5a1ce8c47b4bdbea1aac393720f9d8
xujet <jett.xu@mail.utoronto.ca>
2023-10-17 18:31:47 -0400

    Continued work on api data retrieval

--- index.js
+++ index.js
@@ -11,11 +11,37 @@ $(document).ready(function() {
 
     search_button.addEventListener('click', weatherData);
 
+    /**
+     search_button.addEventListener('click', function() {
+        weather_icon.className = "mw-icon fa-solid fa-circle-exclamation text-light";
+        city.innerHTML = "Whoops! Something went wrong. Please try again later.";
+    });
+     */
+    
+
     function weatherData(){
         const APIKey = '53ff1863fb72f76f754256924527c5cb';
         const city_search = document.querySelector('.city-search').value;
 
-        
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city_search + '&units=metric&appid=53ff1863fb72f76f754256924527c5cb')
+            .then(function (response) {
+                if (response.status !== 200){
+                    weather_icon.className = "mw-icon bi bi-emoji-frown text-light";
+                    city.innerHTML = "Whoops! Something went wrong. Please try again later.";
+                    console.log(response.status);
+                    return;
+                }
+                response.json().then(function (data) {
+                    city.innerHTML = data.name;
+                    feels_like.innerHTML = "Feels like: " + Math.round(data.main.feels_like) + String.fromCharCode(176) + "C";
+                    temperature.innerHTML = Math.round(data.main.temp) + String.fromCharCode(176) + 'C';
+                    console.log(data);
+                    console.log(data.main);
+                    console.log(data.weather[0].main);
+                    console.log(data.name);
+                    console.log(data.main.temp);
+                });
+            });
     }
 
 
