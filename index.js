8696b9d559f718e7ac4b6c9b43407e9879befcfd
xujet <jett.xu@mail.utoronto.ca>
2023-10-18 02:17:46 -0400

    Added changing weather icons and weather displays

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
     const container = document.querySelector('.container');
     const weather = document.querySelector('.weather');
-    const search_city = document.querySelector('.search-city');
     const search_button = document.querySelector('.search-button');
     const weather_icon = document.querySelector('.mw-icon');
     const temperature = document.querySelector('.temperature');
     const city = document.querySelector('.city');
     const feels_like  = document.querySelector('.feels-like');
+    const weather_info = document.querySelector('.weather-info');
+
+    weather_info.style.visibility = "hidden";
+
+    //const wind = document.querySelector('.wind');
+    //const humidity = document.querySeledctor('.humidity');
     
 
     search_button.addEventListener('click', weatherData);
@@ -23,23 +28,50 @@ $(document).ready(function() {
         const APIKey = '53ff1863fb72f76f754256924527c5cb';
         const city_search = document.querySelector('.city-search').value;
 
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city_search + '&units=metric&appid=53ff1863fb72f76f754256924527c5cb')
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city_search + '&units=metric&appid=' + APIKey)
             .then(function (response) {
-                if (response.status !== 200){
-                    weather_icon.className = "mw-icon bi bi-emoji-frown text-light";
+                if (response.status === 404){
+                    weather_icon.className = "mw-icon fa-solid fa-person-circle-question text-light";
+                    city.innerHTML = "Unable to find city. Please try again.";
+                    temperature.innerHTML = "";
+                    feels_like.innerHTML = "";
+                    weather_info.style.visibility = "hidden";
+                    return;
+                } else if (response.status !== 200 && response.status !== 404){
+                    weather_icon.className = "mw-icon fa-solid fa-circle-exclamation text-light";
                     city.innerHTML = "Whoops! Something went wrong. Please try again later.";
+                    temperature.innerHTML = "";
+                    feels_like.innerHTML = "";
+
                     console.log(response.status);
                     return;
                 }
                 response.json().then(function (data) {
                     city.innerHTML = data.name;
-                    feels_like.innerHTML = "Feels like: " + Math.round(data.main.feels_like) + String.fromCharCode(176) + "C";
+                    feels_like.innerHTML = "Feels like: " + Math.round(data.main.feels_like) + String.fromCharCode(176) + 'C';
                     temperature.innerHTML = Math.round(data.main.temp) + String.fromCharCode(176) + 'C';
-                    console.log(data);
-                    console.log(data.main);
+                    weather_info.style.visibility = "visible";
                     console.log(data.weather[0].main);
-                    console.log(data.name);
-                    console.log(data.main.temp);
+                    switch(data.weather[0].main){
+                        case 'Clear':
+                            weather_icon.className = "mw-icon fa-solid fa-sun text-light";
+                            break;
+                        case 'Rain':
+                            weather_icon.className = "mw-icon fa-solid fa-cloud-showers-heavy text-light";
+                            break;
+                        case 'Thunderstorm':
+                            weather_icon.className = "mw-icon fa-solid fa-cloud-bolt text-light";
+                            break;
+                        case 'Drizzle':
+                            weather_icon.className = "mw-icon fa-solid fa-cloud-rain text-light";
+                            break;
+                        case 'Clouds':
+                            weather_icon.className = "mw-icon fa-solid fa-cloud text-light";
+                            break;
+                        case 'Snow':
+                            weather_icon.className = "mw-icon fa-regular fa-snowflake text-light";
+                            break;
+                    }
                 });
             });
     }
