da7816d8df4c291b409c13f178953a517e103837
xujet <jett.xu@mail.utoronto.ca>
2023-10-19 02:30:12 -0400

    Added unit conversion from celsius to fahrenheit

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ $(document).ready(function() {
     const weather_info = document.querySelector('.weather-info');
 
     const wind = document.querySelector('.speed');
-    const humidity = document.querySelector('.percentage');
+    const humidity = document.querySelector('.percentage'); 
 
-    const wind_metric = " m/s";
-    const wind_imperial = " mph";
-    
+    var onC = true;
 
+    //Button elements
+    const celsius = document.querySelector('.celsius');
+    const fahrenheit = document.querySelector('.fahrenheit');
+
+    //Event Listeners for buttons
     search_button.addEventListener('click', weatherData);
+    celsius.addEventListener('click', clickC);
+    fahrenheit.addEventListener('click', clickF);
 
     /**
      search_button.addEventListener('click', function() {
@@ -28,28 +33,42 @@ $(document).ready(function() {
     function weatherData(){
         const APIKey = '53ff1863fb72f76f754256924527c5cb';
         const city_search = document.querySelector('.city-search').value;
+        var units = "";
+        var deg = "";
+        var wind_unit = "";
 
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city_search + '&units=metric&appid=' + APIKey)
+        if (onC === true){
+            units = 'metric';
+            deg = "C";
+            wind_unit = " m/s";
+        }else{
+            units = 'imperial';
+            deg = "F";
+            wind_unit = " mph";
+        }
+
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city_search + '&units=' + units + '&appid=' + APIKey)
             .then(function (response) {
-                if (response.status === 404){
+                if (response.status === 404 || response.status === 400){
                     weather_icon.className = "mw-icon fa-solid fa-person-circle-question text-light";
                     city.innerHTML = "Unable to find city. Please try again.";
-                    temperature.innerHTML = "";
-                    feels_like.innerHTML = "";
+                    weather.style.display = "block";
+                    weather_info.style.display = "none";
                     return;
-                } else if (response.status !== 200 && response.status !== 404){
+                } else if (response.status !== 200 && response.status !== 404 && response.status !== 404){
                     weather_icon.className = "mw-icon fa-solid fa-circle-exclamation text-light";
                     city.innerHTML = "Whoops! Something went wrong. Please try again later.";
-                    temperature.innerHTML = "";
-                    feels_like.innerHTML = "";
-                    console.log(response.status);
+                    weather.style.display = "block";
+                    weather_info.style.display = "none";
                     return;
                 }
                 response.json().then(function (data) {
                     weather.style.display = "block";
+                    weather_info.style.display = "flex";
                     city.innerHTML = data.name;
-                    feels_like.innerHTML = "Feels like: " + Math.round(data.main.feels_like) + String.fromCharCode(176) + 'C';
-                    temperature.innerHTML = Math.round(data.main.temp) + String.fromCharCode(176) + 'C';
+                    
+                    feels_like.innerHTML = "Feels like: " + Math.round(data.main.feels_like) + String.fromCharCode(176) + deg;
+                    temperature.innerHTML = Math.round(data.main.temp) + String.fromCharCode(176) + deg;
                     switch(data.weather[0].main){
                         case 'Clear':
                             weather_icon.className = "mw-icon fa-solid fa-sun text-light";
@@ -70,10 +89,34 @@ $(document).ready(function() {
                             weather_icon.className = "mw-icon fa-regular fa-snowflake text-light";
                             break;
                     }
-                    wind.innerHTML = data.wind.speed + wind_metric;
+                    wind.innerHTML = data.wind.speed + wind_unit;
                     humidity.innerHTML = data.main.humidity + "%";
                 });
             });
+    };
+
+    function clickC(){
+        
+        if (onC === false){
+            onC = true;
+            fahrenheit.style.background = "transparent";
+            celsius.style.background = "#7D6346";
+        }else{
+            return;
+        }
+        
+        
+    }
+
+    function clickF(){
+        if (onC === true){
+            onC = false;
+            celsius.style.background = "transparent";
+            fahrenheit.style.background = "#7D6346";
+        }else{
+            return;
+        }
+        
     }
 
 
